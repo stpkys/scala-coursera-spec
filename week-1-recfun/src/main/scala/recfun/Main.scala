153df4a6ee842d53e7fe23ba6452b0593d851af7
viktor <kudryavtsev.v@gmail.com>
2017-03-25 01:03:01 +0700

    [week-1] no return statement, is it better?

--- week-1-recfun/src/main/scala/recfun/Main.scala
+++ week-1-recfun/src/main/scala/recfun/Main.scala
@@ -9,6 +9,7 @@ object Main {
     def calculate(result: Int, current: Int): Integer = {
       if (current == 1) result else calculate(result * n, n - 1)
     }
+
     calculate(1, n)
   }
 
@@ -17,6 +18,7 @@ object Main {
       if (a > b) acc
       else loop(a + 1, acc + f(a))
     }
+
     loop(a, 0)
   }
 
@@ -26,15 +28,15 @@ object Main {
   }
 
   def sum1(f: Int => Int)(a: Int, b: Int): Int = {
-//    def sumf(a: Int, b: Int): Int = {
-//      if (a > b) 0
-//      else f(a) + sumf(a+1, b)
-//    }
-//    return sumf
-    if (a > b) 0 else  f(a) + sum1(f)(a + 1, b)
+    //    def sumf(a: Int, b: Int): Int = {
+    //      if (a > b) 0
+    //      else f(a) + sumf(a+1, b)
+    //    }
+    //    return sumf
+    if (a > b) 0 else f(a) + sum1(f)(a + 1, b)
   }
 
-  def genFunc(comb: (Int, Int) => Int, base: Int)(f: Int => Int)(a: Int, b:Int) : Int =
+  def genFunc(comb: (Int, Int) => Int, base: Int)(f: Int => Int)(a: Int, b: Int): Int =
     if (a > b) base
     else comb(f(a), genFunc(comb, base)(f)(a + 1, b))
 
@@ -42,42 +44,40 @@ object Main {
     def prod(a: Int, b: Int) = genFunc((x: Int, y: Int) => x * y, 1)((d) => d)(a, b)
 
     def factorial(n: Int): Int = product(x => x)(1, n)
+
     println(product(x => x)(1, 5))
     println(prod(1, 5))
     println(factorial(4))
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = {
-      if (c < 1 || c >= r) return 1
-      return pascal(c-1, r-1) + pascal(c, r-1)
-    }
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = {
-      def test(current: List[Char], count: Int) : Boolean = {
-        if (current.isEmpty) return count == 0
-        if (count < 0) return false
-        if (current.head == '(') return test(current.tail, count + 1)
-        if (current.head == ')') {
-          return test(current.tail, count - 1)
-        }
-        test(current.tail, count)
-      }
-      if (chars.isEmpty) return true
-      return test(chars, 0)
-    }
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = {
-      if (coins.isEmpty || money < 0) return 0
-      if (money == coins.head) return 1 + countChange(money, coins.tail)
-      countChange(money - coins.head, coins) + countChange(money, coins.tail)
-    }
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c < 1 || c >= r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def test(current: List[Char], count: Int): Boolean = {
+      if (current.isEmpty) count == 0
+      else if (count < 0) false
+      else if (current.head == '(') test(current.tail, count + 1)
+      else if (current.head == ')') test(current.tail, count - 1)
+      else test(current.tail, count)
+    }
+
+    test(chars, 0)
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty || money < 0) 0
+    else if (money == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
+}
