08f76a44f9a86b44995c402eabc3511dc85663af
viktor <kudryavtsev.v@gmail.com>
2017-03-25 19:38:17 +0700

    Initial commit

---
+++ week-1-scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -0,0 +1,79 @@
+package scalashop
+
+import java.util.concurrent.ForkJoinTask
+
+import org.scalameter._
+import common._
+
+
+object VerticalBoxBlurRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      VerticalBoxBlur.blur(src, dst, 0, width, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      VerticalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+
+}
+
+/** A simple, trivially parallelizable computation. */
+object VerticalBoxBlur {
+
+  /** Blurs the columns of the source image `src` into the destination image
+   *  `dst`, starting with `from` and ending with `end` (non-inclusive).
+   *
+   *  Within each column, `blur` traverses the pixels by going from top to
+   *  bottom.
+   */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    var x = from
+    while(x < end) {
+      var y = 0
+      while(y < src.height) {
+        dst(x, y) = boxBlurKernel(src, x, y, radius)
+        y += 1
+      }
+      x += 1
+    }
+  }
+
+  /** Blurs the columns of the source image in parallel using `numTasks` tasks.
+   *
+   *  Parallelization is done by stripping the source image `src` into
+   *  `numTasks` separate strips, where each strip is composed of some number of
+   *  columns.
+   */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    val batch = src.width / numTasks + (if ((src.width % numTasks) > 0) 1 else 0)
+    val tasks: Array[ForkJoinTask[Unit]] = new Array[ForkJoinTask[Unit]](numTasks)
+    for(i <- 0 until numTasks) {
+      tasks(i) = task {
+        blur(src, dst, i * batch, clamp((i + 1) * batch, 0, src.width), radius)
+      }
+    }
+    for(i <- tasks) {
+      i.join()
+    }
+  }
+
+}
---
+++ week-1-scalashop/src/main/scala/scalashop/package.scala
@@ -0,0 +1,62 @@
+
+
+package object scalashop {
+
+  /** The value of every pixel is represented as a 32 bit integer. */
+  type RGBA = Int
+
+  /** Returns the red component. */
+  def red(c: RGBA): Int = (0xff000000 & c) >>> 24
+
+  /** Returns the green component. */
+  def green(c: RGBA): Int = (0x00ff0000 & c) >>> 16
+
+  /** Returns the blue component. */
+  def blue(c: RGBA): Int = (0x0000ff00 & c) >>> 8
+
+  /** Returns the alpha component. */
+  def alpha(c: RGBA): Int = (0x000000ff & c) >>> 0
+
+  /** Used to create an RGBA value from separate components. */
+  def rgba(r: Int, g: Int, b: Int, a: Int): RGBA = {
+    (r << 24) | (g << 16) | (b << 8) | (a << 0)
+  }
+
+  /** Restricts the integer into the specified range. */
+  def clamp(v: Int, min: Int, max: Int): Int = {
+    if (v < min) min
+    else if (v > max) max
+    else v
+  }
+
+  /** Image is a two-dimensional matrix of pixel values. */
+  class Img(val width: Int, val height: Int, private val data: Array[RGBA]) {
+    def this(w: Int, h: Int) = this(w, h, new Array(w * h))
+    def apply(x: Int, y: Int): RGBA = data(y * width + x)
+    def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
+  }
+
+  /** Computes the blurred RGBA value of a single pixel of the input image. */
+  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
+    if (radius == 0) return src(x, y)
+    var r = 0
+    var g = 0
+    var b = 0
+    var a = 0
+    var count = 0
+
+    for(dx <- clamp(x - radius, 0, src.width) until clamp(x + radius + 1, 0, src.width)) {
+      for(dy <- clamp(y - radius, 0, src.height) until clamp(y + radius + 1, 0, src.height)) {
+        val pix = src(dx, dy)
+        r += red(pix)
+        g += green(pix)
+        b += blue(pix)
+        a += alpha(pix)
+        count += 1
+      }
+    }
+
+    rgba(r / count, g / count, b / count, a / count)
+  }
+
+}
